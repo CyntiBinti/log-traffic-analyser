783826ac8ce3afdd24c1edf4f19828134ecb6996
CyntiBinti <cynthia.mbulu@ft.com>
2025-04-23 23:40:54 +0100

    test: add jest.config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,26 @@
 module.exports = {
-	extends: ['js/recommended', 'plugin:prettier/recommended'],
+	extends: [
+		'js/recommended',
+		'plugin:prettier/recommended',
+		'plugin:jsdoc/recommended'
+	],
 	parser: '@babel/eslint-parser',
 	parserOptions: {
 		requireConfigFile: false,
 		sourceType: 'script'
 	},
-	plugins: ['prettier'],
+	plugins: ['prettier', 'jsdoc'],
 	rules: {
+		indent: 'off',
+		'jsdoc/check-access': 'error',
+		'jsdoc/no-undefined-types': ['error', { definedTypes: ['Iterable'] }],
+		'jsdoc/require-description-complete-sentence': 'off',
+		'jsdoc/require-jsdoc': 'off',
+		'jsdoc/require-param-description': 'off',
+		'jsdoc/require-returns-description': 'off',
+		'jsdoc/tag-lines': ['warn', 'any', { startLines: 1 }],
 		'prettier/prettier': 'error',
 		quotes: 0,
 		'space-before-function-paren': 0
-	},
-	ignorePatterns: ['tmp/**']
+	}
 };
